7d059eeaf50bc84e7f07cf6318e7d211b2669150
LykourgosS <lpsarantidis@gmail.com>
2019-07-10 23:45:37 +0300

    Initial commit - CodingInFlow Services & Background Tasks (2/13)

---
+++ app/src/main/java/com/unipi/lykourgoss/earthquakeobserver/codinginflow/servicesandbackground/b_ForegroundService/App.java
@@ -0,0 +1,38 @@
+package com.unipi.lykourgoss.earthquakeobserver.codinginflow.servicesandbackground.b_ForegroundService;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+/**
+ * Created by LykourgosS <lpsarantidis@gmail.com>
+ * on 10,July,2019.
+ */
+
+public class App extends Application {
+
+    public static final String CHANNEL_ID = "exampleServiceChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){ // notification channels are available for API v.26 and up
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Channel name",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            // if make any changes (i.e. notification's behavior) here uninstall and re-install the app
+            serviceChannel.setDescription("This is channel's description");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+}
---
+++ app/src/main/java/com/unipi/lykourgoss/earthquakeobserver/codinginflow/servicesandbackground/b_ForegroundService/ForegroundServiceActivity.java
@@ -0,0 +1,65 @@
+package com.unipi.lykourgoss.earthquakeobserver.codinginflow.servicesandbackground.b_ForegroundService;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.unipi.lykourgoss.earthquakeobserver.R;
+
+public class ForegroundServiceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXTRA_INPUT =
+            "com.unipi.lykourgoss.earthquakeobserver.codinginflow.servicesandbackground.b_ForegroundService.EXTRA_INPUT";
+
+    private EditText editTextInput;
+    private Button buttonStartService;
+    private Button buttonStopService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foreground_service);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+
+        buttonStartService = findViewById(R.id.button_start_service);
+        buttonStartService.setOnClickListener(this);
+
+        buttonStopService = findViewById(R.id.button_stop_service);
+        buttonStopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_start_service:
+                startService();
+                break;
+            case R.id.button_stop_service:
+                stopService();
+                break;
+        }
+    }
+
+    private void startService() {
+        String input = editTextInput.getText().toString().trim();
+
+        Intent intentService = new Intent(this, ExampleService.class);
+        intentService.putExtra(EXTRA_INPUT, input);
+
+        // to start the service while app is on the background call
+        // startForegroundService(intentService), but after 5 seconds max should call
+        // startForeground(...) within Service onStartCommand()!!!
+        ContextCompat.startForegroundService(this, intentService);
+    }
+
+    private void stopService() {
+        Intent intentService = new Intent(this, ExampleService.class);
+        stopService(intentService);
+    }
+}
